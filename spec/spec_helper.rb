f02bab6a76887dced61693a0866c86361a851d70
Kerry Buckley <kerryjbuckley@gmail.com>
2010-08-20 07:24:30 +0100

    Require rubygems when running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 $:.unshift File.expand_path("../../lib", __FILE__)
+require "rubygems"
 require "spec"
 require "curb"
 require "stringio"
 require "redrock"
-include RedRock
 require "webmock/test_unit"
 require "webmock/rspec"
+include RedRock
